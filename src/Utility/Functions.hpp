b00e1148781f0b4e354479fb551dcb28b1548761
ViktorBusk <bviktor228@gmail.com>
2020-04-02 20:57:59 +0200

    Basic filestructure

--- src/Utility/Functions.hpp
+++ src/Utility/Functions.hpp
@@ -5,12 +5,16 @@ namespace util
 {
 	struct fn
 	{
-		static float distance(sf::Vector2f p1, sf::Vector2f p2)
+		static float distance2i(const sf::Vector2i& a, const sf::Vector2i& b)
+		{
+			return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+		}
+		static float distance2f(const sf::Vector2f &p1, const sf::Vector2f &p2)
 		{
 			return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 		}
 
-		static void zoomViewAt(sf::Vector2i pixel, sf::RenderWindow& window, float zoom)
+		static void zoomViewAt(const sf::Vector2i &pixel, sf::RenderWindow& window, const float &zoom)
 		{
 			const sf::Vector2f beforeCoord { window.mapPixelToCoords(pixel) };
 			sf::View view { window.getView() };
@@ -47,6 +51,16 @@ namespace util
 				value = minimum;
 			else if (value > maximum)
 				value = maximum;
+		}
+		static bool AABBColision(const sf::RectangleShape& r1, const sf::RectangleShape& r2)
+		{
+			sf::Vector2f r1Pos = r1.getPosition();
+			sf::Vector2f r1Size(r1.getGlobalBounds().width, r1.getGlobalBounds().height);
+
+			sf::Vector2f r2Pos = r2.getPosition();
+			sf::Vector2f r2Size(r2.getGlobalBounds().width, r2.getGlobalBounds().height);
+
+			return (r1Pos.x < r2Pos.x + r2Size.x && r1Pos.x + r1Size.x > r2Pos.x && r1Pos.y < r2Pos.y + r2Size.y && r1Pos.y + r1Size.y > r2Pos.y);
 		}
 	};
 }
