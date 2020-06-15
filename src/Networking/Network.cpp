96e67df7737ebc09b19d7582cde150c2fd925838
Axel Gard <axel.gard@protonmail.com>
2020-06-15 15:56:27 +0200

    cli and ser passing msg

--- src/Networking/Network.cpp
+++ src/Networking/Network.cpp
@@ -5,7 +5,8 @@
 int main(){
 	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
 	sf::TcpSocket socket;
-	char connectionType, mode;
+	char connectionType;
+	char mode = 'r';
 
 	short int port = 25565;
 	char buffer[2000];
@@ -21,17 +22,38 @@ int main(){
 		listener.listen(port);
 		listener.accept(socket);
 		text += "server ";
+		mode = 's';
 	}
 	else if(connectionType == 'c'){
 		socket.connect(ip, port);
 		text += " client";
+		mode = 'r';
 	}
 
 	socket.send(text.c_str(), text.length() +1);
 
 	socket.receive(buffer, sizeof(buffer), received);
-
 	std::cout << buffer << std::endl;
+
+	bool done = false;
+
+	while (!done){
+		if(mode == 's'){
+			std::getline(std::cin, text);
+			socket.send(text.c_str(), text.length() +1);
+			mode = 'r';
+		}
+		else if (mode == 'r')
+		{
+			socket.receive(buffer, sizeof(buffer), received);
+			if(received > 0){
+				std::cout << buffer << std::endl;
+				mode = 's';
+			}
+
+		}
+
+	}
 
 
 	return 0;
--- src/Networking/Network.h
+++ src/Networking/Network.h
@@ -1,7 +1,19 @@
-#ifndef Network
-#define Network
+#ifndef NETWORK
+#define NETWORK
 
 #include <SFML/Network.hpp>
 #include <iostream>
 #include <string.h>
+
+class Network
+{
+private:
+
+
+public:
+
+};
+
+
+
 #endif
