f32b7999b197585d7f8d27ab110dad69e6f4b433
Axel Gard <axel.gard@protonmail.com>
2020-06-16 17:09:36 +0200

    UDP smoth postion

--- src/Networking/Network.cpp
+++ src/Networking/Network.cpp
@@ -4,70 +4,43 @@ sf::Mutex globalMutex;
 sf::RectangleShape rect1, rect2;
 sf::UdpSocket socket;
 sf::IpAddress sendIp = "";
-unsigned short port = 25565;
+unsigned short port = 24474;
 bool quit = false;
+float dt;
+sf::Clock gameClock;
 
 void handlePacketTraffic(void)
 {
 	static sf::Vector2f prevPosition, p2Position;
+	static sf::Clock clock;
+	clock.restart().asMilliseconds();
 	while(!quit)
 	{
 		sf::Packet packet;
-
-		//Send packet
-		globalMutex.lock();
-		if (prevPosition != rect1.getPosition())
-			packet << rect1.getPosition().x << rect1.getPosition().y;
-		globalMutex.unlock();
-
-		socket.send(packet, sendIp, port);
-
-		//Receive packet
-		socket.receive(packet, sendIp, port);
-		if(packet >> p2Position.x >> p2Position.y)
+		if(clock.getElapsedTime().asMilliseconds() >= 30)
 		{
+			clock.restart().asMilliseconds();
+			//Send packet
 			globalMutex.lock();
-			rect2.setPosition(p2Position);
-			prevPosition = rect1.getPosition();
+			if (prevPosition != rect1.getPosition())
+				packet << rect1.getPosition().x << rect1.getPosition().y;
 			globalMutex.unlock();
+
+			socket.send(packet, sendIp, port);
+			//Receive packet
+			socket.receive(packet, sendIp, port);
+			if (packet >> p2Position.x >> p2Position.y)
+			{
+				globalMutex.lock();
+
+				rect2.setPosition(p2Position);
+				prevPosition = rect1.getPosition();
+				globalMutex.unlock();
+			}
 		}
 	}
 }
-/*
-// ----- The client -----
-// Create a socket and bind it to the port 55001
-sf::UdpSocket socket;
-socket.bind(55001);
-// Send a message to 192.168.1.50 on port 55002
-std::string message = "Hi, I am " + sf::IpAddress::getLocalAddress().toString();
-socket.send(message.c_str(), message.size() + 1, "192.168.1.50", 55002);
 
-// Receive an answer (most likely from 192.168.1.50, but could be anyone else)
-char buffer[1024];
-std::size_t received = 0;
-sf::IpAddress sender;
-unsigned short port;
-
-socket.receive(buffer, sizeof(buffer), received, sender, port);
-std::cout << sender.ToString() << " said: " << buffer << std::endl;
-// ----- The server -----
-// ----- The server -----
-// Create a socket and bind it to the port 55002
-sf::UdpSocket socket;
-socket.bind(55002);
-// Receive a message from anyone
-char buffer[1024];
-
-std::size_t received = 0;
-sf::IpAddress sender;
-unsigned short port;
-
-socket.receive(buffer, sizeof(buffer), received, sender, port);
-std::cout << sender.ToString() << " said: " << buffer << std::endl;
-// Send an answer
-std::string message = "Welcome " + sender.toString();
-socket.send(message.c_str(), message.size() + 1, sender, port);
-*/
 
 int main(){
 	sf::Thread* thread = 0;
@@ -80,7 +53,7 @@ int main(){
 	//std::map<unsigned short, sf:IpAddress> computerID;
 
 	socket.bind(port);
-	//socket.setBlocking(false);
+	socket.setBlocking(false);
 
 	if(connectionType == 's'){
 		bool receviedIp = false;
@@ -115,7 +88,7 @@ int main(){
 
 	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "Packets");
 
-	socket.setBlocking(false);
+	//socket.setBlocking(false);
 
 	thread = new sf::Thread(&handlePacketTraffic);
 	thread->launch();
@@ -130,6 +103,18 @@ int main(){
 				window.close();
 			}
 		}
+
+		dt = gameClock.restart().asSeconds();
+		float FPS = 1.f / dt;
+		static float incer = 0;
+		static float cap = 5;
+		incer+=dt;
+		if(incer >= cap)
+		{
+			incer = 0;
+			window.setTitle("Forest Valley | FPS: " + std::to_string(FPS));
+		}
+
 		globalMutex.lock();
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 			rect1.move(0.1f, 0.0f);
@@ -154,101 +139,4 @@ int main(){
 	}
 	return 0;
 }
-
-
-/*
-#include <SFML/Network.hpp>
-
-const unsigned short PORT = 5000;
-const std::string IPADDRESS("192.168.0.100");//change to suit your needs
-
-std::string msgSend;
-
-sf::TcpSocket socket;
-sf::Mutex globalMutex;
-bool quit = false;
-
-void DoStuff(void)
-{
-	static std::string oldMsg;
-	while(!quit)
-	{
-		sf::Packet packetSend;
-		globalMutex.lock();
-		packetSend << msgSend;
-		globalMutex.unlock();
-
-		socket.send(packetSend);
-
-		std::string msg;
-		sf::Packet packetReceive;
-
-		socket.receive(packetReceive);
-		if ((packetReceive >> msg) && oldMsg != msg && !msg.empty())
-		{
-			std::cout << msg << std::endl;
-			oldMsg = msg;
-		}
-	}
-}
-
-void Server(void)
-{
-	sf::TcpListener listener;
-	listener.listen(PORT);
-	listener.accept(socket);
-	std::cout << "New client connected: " << socket.getRemoteAddress() << std::endl;
-}
-
-bool Client(void)
-{
-	if(socket.connect(IPADDRESS, PORT) == sf::Socket::Done)
-	{
-		std::cout << "Connected\n";
-		return true;
-	}
-	return false;
-}
-
-void GetInput(void)
-{
-	std::string s;
-	std::cout << "\nEnter \"exit\" to quit or message to send: ";
-	getline(std::cin,s);
-	if(s == "exit")
-		quit = true;
-	globalMutex.lock();
-	msgSend = s;
-	globalMutex.unlock();
-}
-
-
-int main(int argc, char* argv[])
-{
-	sf::Thread* thread = 0;
-
-	char who;
-	std::cout << "Do you want to be a server (s) or a client (c) ? ";
-	std::cin  >> who;
-
-	if (who == 's')
-		Server();
-	else
-		Client();
-
-	thread = new sf::Thread(&DoStuff);
-	thread->launch();
-
-	while(!quit)
-	{
-		GetInput();
-	}
-
-	if(thread)
-	{
-		thread->wait();
-		delete thread;
-	}
-	return 0;
-}*/
 
